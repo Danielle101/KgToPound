a8fb9a9cd370c86e696545b6940a8e166b7f69a1
dh <halldan4@yahoo.com>
2016-05-24 23:55:43 -0400

    added conversion formula for lbs to kg and continuation and exit prompts

--- src/kgToPound/KgToPound.java
+++ src/kgToPound/KgToPound.java
@@ -2,8 +2,6 @@ package kgToPound;
 
 import java.util.Scanner;
 
-import inchConverter.Validator;
-
 public class KgToPound {
 
 	public static void main(String[] args) {
@@ -20,12 +18,22 @@ public class KgToPound {
 			System.out.println("\nPlease enter a number (1 or 2):");
 			int userSelect = Validator.getValidInt(1, 2);
 			if (userSelect == 1) {
-				System.out.println("Please enter your measurement in kilograms:");
+				System.out.println("Please enter the weight  in kilograms:");
 				double userKilo = Validator.readValidInt();
-				double LBSCon = userKilo * 2.2;
+				double LBSCon = userKilo * 2.2046;
 				System.out.println(userKilo + " kilograms is equivalent to " + LBSCon + " pounds.");
-			
-	}
+			} else if (userSelect == 2) {
+				System.out.println("Please enter the weight in pounds:");
+				double userLbs = Validator.readValidInt();
+				double KiloCon = userLbs/2.2046;
+				System.out.println(userLbs + " pounds is equivalent to " + KiloCon+ " kilograms.");
+			}
 
+			choice = Validator.getString ("\nWould you like to convert another number? (y/n):\n");
+		}
+	//exit message
+	System.out.println("Thanks for using the KgToPound Conversion App! Come back soon!");
+	sc.close();
+	}
 }
-}
+
